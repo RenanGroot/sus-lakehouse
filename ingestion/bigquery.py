9aa83de8061b93bd62455e663309fecde24c6100
RenanGroot <renangroot@hotmail.com>
2026-06-09 18:49:00 -0300

    refactor: use env variables and add docstrings in bigquery.py

--- ingestion/bigquery.py
+++ ingestion/bigquery.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
+import os
 import logging
 
 load_dotenv()
@@ -10,23 +11,36 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+project_id = os.getenv("GCP_PROJECT_ID")
+table_id = f"{project_id}.sih_raw.SP24"
+bucket_name = os.getenv("BUCKET_NAME")
+
 client = bigquery.Client()
-
-table_id = "project-8c33836c-e0e4-4c1d-9d8.sih_raw.SP24"
 external_source_format = bigquery.ExternalSourceFormat.PARQUET
-source_uris = ["gs://project-8c33836c-e0e4-4c1d-9d8-sus-lakehouse-raw/*.parquet"]
+source_uris = [f"gs://{bucket_name}/*.parquet"]
 
-def create_table():
+def create_table(target_table: str) -> None:
+    """
+    Connects to BigQuery Client, create a new external source 
+    table configured for Parquet files, skips if it already exists.
+    
+    Args:
+        target_table: Table ID to be created
+
+    Returns:
+        None
+    """
     try:
-        client.get_table(table_id)
+        client.get_table(target_table)
         logging.warning("Table already exists, skipping")
+
     except NotFound:
         external_config = bigquery.ExternalConfig(external_source_format)
         external_config.source_uris = source_uris
-        table = bigquery.Table(table_id)
+        table = bigquery.Table(target_table)
         table.external_data_configuration = external_config
         client.create_table(table)
         logging.info("Table created sucessfully!")
 
 if __name__ == "__main__":
-    create_table()
+    create_table(table_id)
